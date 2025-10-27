f3e6c41ae814903404da5cb75239af66c9bc3f43
Kamenskaya <domino_309@mail.ru>
2025-10-27 21:14:38 +0500

    практическая работа

---
+++ tests/lection.py
@@ -0,0 +1,33 @@
+# Задание 1.
+# x = int(input("Число: "))
+
+# if x % 2 == 0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+# Задание 2.
+# x = int(input("Число: "))
+#
+# if x % 7 == 0:
+#      print("Number is multiple 7")
+# else:
+#      print("Number is not multiple 7")
+
+# Задание 3.
+# x = int(input("Число_1: "))
+# y = int(input("Число_2: "))
+
+# if x > y == 0:
+#      print("x")
+# else:
+#     print("y")
+
+# Задание 4.
+# x = int(input("Число_1: "))
+# y = int(input("Число_2: "))
+#
+# if x > y == 0:
+#     print("y")
+# else:
+#     print("x")
