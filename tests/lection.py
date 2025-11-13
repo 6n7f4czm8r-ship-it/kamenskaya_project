eb8d5bb4a136d7188e518ae807e0bc8a5489d812
1glb1 <glebkrot200800@gmail.com>
2025-11-13 21:12:47 +0500

    13/11/25

--- tests/lection.py
+++ tests/lection.py
@@ -53,8 +53,25 @@
 # for x in range(1, x+1):
 #     print(x)
 
-x = int(input("Число: "))
-counter = 1
-while counter != x+1:
-    print(1 + counter)
-    counter=counter + 1
+# x = int(input("Число: "))
+# counter = 1
+# while counter != x+1:
+#     print(1 + counter)
+#     counter=counter + 1
+
+# numbers = [1,2,3,4,5,6]
+# numbers.append(7)
+# numbers.insert(0, 0)
+# print(numbers)
+#
+# numbers = [1,2,3,4,5,6]
+# numbers.pop(0)
+# numbers.pop(5)
+# print(numbers)
+
+# numbers = [1,2,3,4,5,6]
+# x = int(input("Число: "))
+# if x in numbers:
+#     print("Число в списке")
+# else:
+#     print("Число не в списке")
