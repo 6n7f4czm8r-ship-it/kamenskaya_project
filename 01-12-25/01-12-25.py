d58e02b42c9aa12be7910db25552a6d7595676f7
Egitva <9026479948e@gmail.com>
2025-12-01 21:14:11 +0500

    практическая работа 01/12

---
+++ 01-12-25/01-12-25.py
@@ -0,0 +1,46 @@
+#1
+# def jobs():
+#     print("“Don't let the noise of others' opinions")
+#     print("drown out your own inner voice.“")
+#     print("                                Steve Jobs")
+# jobs()
+from tabnanny import check
+
+
+#2
+# def numbers(x,y):
+#     check=[]
+#     if x > y:
+#         for i in range (y,x):
+#             if i % 2!=0:
+#                 check.append(i)
+#         print(check)
+#     else:
+#         for i in range (x,y):
+#             if i % 2!=0:
+#                 check.append(i)
+#         print(check)
+#
+# numbers(90,15)
+
+#4
+# def numbers(a,b,c,d):
+#     check=[]
+#     check.append(a)
+#     check.append(b)
+#     check.append(c)
+#     check.append(d)
+#     print(max(check))
+#
+# numbers(5,6,8,10)
+
+#5
+# def numbers(a,b):
+#     check=[]
+#     check.append(a)
+#     check.append(b)
+#     return (sum(check))
+#
+# a = numbers(10,222)
+# print(a)
+
