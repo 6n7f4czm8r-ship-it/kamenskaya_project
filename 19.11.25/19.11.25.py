4cd6b1b55f620c627e24116858abaf1fd5b70e73
Egitva <9026479948e@gmail.com>
2025-12-03 21:24:04 +0500

    практическая работа 19/11

---
+++ 19.11.25/19.11.25.py
@@ -0,0 +1,42 @@
+    #1
+# 1.1
+
+
+# 1.2.
+# text = input("Текст: ")
+# number = 0
+# for symb in text:
+#     if symb.isdigit():
+#         number +=1
+#         print(f"Цифр: {number}")
+#     else:
+#         print(f"Только текст")
+
+# 1.3
+# text = input("Текст: ")
+# text = "Текст Да Это текст но еще и цифры 1 2 3"
+# count = 0
+# for symb in text:
+#     if symb.isalpha() is False and symb.isdigit() is False and symb.isspace() is False:
+#         count +=1
+# print(f"Знаков препинания: {count}")
+
+# 1.4
+# text = input("Текст: ")
+# count = 0
+# for symb in text:
+#     if symb.isalpha() is False:
+#         count +=1
+# print(f"Знаков восклицания: {count}")
+
+#2
+numbers = input("Последовательность: ")
+numb_list = numbers.split(" ")
+num = input("Нужное число: ")
+count = 0
+for numb in numb_list:
+    if numb == num:
+        count +=1
+print(f"Счетчик: {count}")
+
+
