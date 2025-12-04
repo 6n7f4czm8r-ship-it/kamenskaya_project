faf37adc37d864e0093654d3d1a8ad807a3e69ee
student <domino_309@mail.ru>
2025-12-04 21:16:50 +0500

    практическая от 04.12

---
+++ 04-12-25/04-12-25.py
@@ -0,0 +1,26 @@
+#1
+# x = int(input("Число_1: "))
+# y = int(input("Число_2: "))
+# numbers = range(x, y+1)
+# b = len(numbers)
+# a = sum(range(x, y+1))
+# # print(a)
+# c = a // b
+# print(c)
+
+#2
+# x = 1
+# y = int(input("Число_1: "))
+# for i in range(1, y+1):
+#     x *= i
+# print(x)
+
+#3
+# x = int(input("Число: "))
+# symb = "*"*x
+# print(symb)
+
+# #4
+# x = int(input("Число: "))
+# symb = "&"*x
+# print(symb)
