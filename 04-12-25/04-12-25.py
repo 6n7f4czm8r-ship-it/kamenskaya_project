5c8c993d24338a232f0d21f5cc48d20bbe047b1a
Egitva <9026479948e@gmail.com>
2025-12-15 20:50:04 +0500

    практическая работа 13/11

--- 04-12-25/04-12-25.py
+++ 04-12-25/04-12-25.py
@@ -7,6 +7,7 @@
 # # print(a)
 # c = a // b
 # print(c)
+from random import randint
 
 #2
 # x = 1
@@ -32,27 +33,39 @@
 #     print (rez)
 
 #3
-x = 5
+# x = 5
 # x = int(input("Число для поиска: "))
 # a = int(input("Начало диапазона: "))
 # b = int(input("Конец диапазона: "))
-a = 1
-b = 6
-num_in_list = False
-numbers = []
-# numbers_1 = [1, 2, 3, 4, 5, 6]
-for i in range (a, b+1):
-    numbers.append(i)
-    if x in numbers:
-        num_in_list = True
-if not num_in_list:
-    print("Числа нет")
+# num_in_list = False
+# numbers = []
+# # numbers_1 = [1, 2, 3, 4, 5, 6]
+# for i in range (a, b+1):
+#     numbers.append(i)
+#     if x in numbers:
+#         num_in_list = True
+# if not num_in_list:
+#     print("Числа нет")
 
 
-
-print(numbers)
-# for y in numbers:
-#     if x == y:
+#4
+# import random
+# random_integer = random.randint(0, 500)
+# print(random_integer)
+# x = randint(1,500)
+# print(x)
+#
+# for a in range (1,500):
+#     y = int(input("Число пользователя: "))
+#     if a == x:
+#         print ("Вы угадали число")
+#     elif a > x:
+#         print("Число меньше твоего")
+#     elif y == 0:
+#         print("стоп")
+#         break
+#     else:
+#         print("Число больше твоего")
 
 
 
