d5b07d483e058fdaf44683fc51e565903bbad983
1glb1 <glebkrot200800@gmail.com>
2026-01-15 21:13:31 +0500

    15.01.2026

---
+++ 15-01-26/15-01-26.py
@@ -0,0 +1,21 @@
+#1
+# def text():
+#     print ("""`Don`t compare yourself with anyone in this world…
+# if you do so, you are insulting yourself`
+# Bill Gates""")
+#
+# text()
+
+#2
+# def numb (x,y):
+#     z = []
+#     for i in range (x, y+1):
+#         if i %2==0:
+#             z.append(i)
+#     print(z)
+#
+# x = int(input("Первое число: "))
+# y = int(input("Второе число: "))
+# numb(x, y)
+
+#3
